e4e99a7f73cb8b545919be769903128c0d74fe32
Matthew Stevenson <stevensonmt@gmail.com>
2016-11-13 12:39:52 -0800

    Make TODO.txt list

--- scotch-blog/config/routes.rb
+++ scotch-blog/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  root to: "posts#index"
+  root 'posts#landing_page'
+  get '/index', to: 'posts#index'
+  # get '/login', to: 'sessions#new'
+  # post '/login', to: 'sessions#create'
+  # delete '/logout', to 'sessions#destroy'
 
   resources :posts
+  resources :authors
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :posts, only: [:create, :destroy]
+
 end
